b8046c7263a52f181e082d555dd90acd82eb3cd9
+ <+>
2023-03-03 14:14:25 +0300

    :tada: Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { RouterProvider } from "react-router-dom";
+
+export default function App() {
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
+}
+
+
